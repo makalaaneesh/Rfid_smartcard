64db22e9022e56636cba2c5fd5c4562b7fa840fc
makalaaneesh <makalaaneesh@yahoo.com>
2015-01-11 15:33:07 +0530

    final changes1

---
+++ rfid/manager/templates/manager/js/transition.js
@@ -0,0 +1,4 @@
+function transitionToPage(sNewPage){
+    // insert your transition out fade effect code here
+    window.location = sNewPage;
+}
--- rfid/manager/views.py
+++ rfid/manager/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
-from manager.models import StudentData
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from manager.models import StudentData, Mess
 
 
 def index(request):
@@ -16,12 +16,40 @@ def enter(request):
 def detail(request, rollnumber):
 	student= get_object_or_404(StudentData, roll_number=rollnumber)
 	cols=list(student._meta.get_all_field_names())
-	
+	# indices=[0,2,4,5,7,8,9,10,12,13,15,17,20,21]
+	indices=[1,3,6,11,14,16,18,19]
+
+	cols1=[]
+	for i in indices:
+		cols1.append(cols[i])
 	values=[]
-	for col in cols:
+	for col in cols1:
 		values.append(getattr(student, col))
-	comb= zip(cols, values)
-	context={'student':student, 'cols':cols,'values':values, 'comb':comb }
+	aa=0
+	d=""
+	strk=0
+	guest=0
+	row=Mess.objects.filter(roll_number=rollnumber).count()
+	if row==0:
+		a=0
+	else:
+		a=Mess.objects.get(roll_number=rollnumber)
+		aa=a.attendance
+		d = a.date
+		strk = a.streak
+		guest = a.guest
+
+
+
+	# try:
+	# 	row=Mess.objects.get(roll_number=rollnumber)
+ #    except Mess.DoesNotExist:
+ #    						raise Http404
+        # raise Http404
+	comb= zip(cols1, values)
+	dues = aa * 25
+	totdues = dues + (guest * 50)
+	context={'student':student, 'dues': dues, 'cols':cols,'attendance':aa, 'totdues':totdues, 'comb':comb, 'streak' :strk, 'lmeal' :d, 'guest':guest }
 	return render(request, 'manager/detail1.html', context)
 
 def entered(request):
